081cfb2c9c367ceaa2a40cd5001ed57f64751447
Parth <pbhtngr@icloud.com>
2025-06-08 16:10:22 +0400

    Changed starting camera angle, split scene setup to extension trait

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod types;
 mod utils;
 
 use anyhow::Result;
+use kiss3d::camera::ArcBall;
 use std::{cell::RefCell, rc::Rc};
 
-use kiss3d::nalgebra::Vector3;
+use kiss3d::nalgebra::{Point3, Unit, Vector3};
 use kiss3d::window::{CanvasSetup, Window};
 
 use crate::plot::construct_mesh;
@@ -14,6 +15,8 @@ use crate::render::Render;
 use crate::types::{DeribitDataPoint, DeribitWebSocketMessage};
 
 fn main() -> Result<()> {
+    let (mut window, mut camera) = Window::window_and_camera_set_up();
+
     // Test data
     let raw_options = serde_json::from_str::<DeribitWebSocketMessage>(include_str!(
         "../resources/test_data.json"
@@ -28,24 +31,14 @@ fn main() -> Result<()> {
         .map(|option| option.into_data_point())
         .collect();
 
-    let mut window = Window::new_with_setup(
-        "Volatility Surface Mesh",
-        800,
-        600,
-        CanvasSetup {
-            samples: kiss3d::window::NumSamples::Eight,
-            vsync: true,
-        },
-    );
-
     let mesh = construct_mesh(deribit_points);
     let mut mesh_node = window.add_mesh(Rc::new(RefCell::new(mesh)), Vector3::new(1.0, 1.0, 1.0));
 
     mesh_node.recompute_normals();
-    mesh_node.set_color(0.3, 1.0, 1.0);
+    mesh_node.set_color(0.4, 0.7, 1.0);
 
     window.set_light(kiss3d::light::Light::StickToCamera);
-    while window.render() {
+    while window.render_with_camera(&mut camera) {
         window.draw_axes();
 
         // mesh_node.recompute_normals();
--- src/render.rs
+++ src/render.rs
@@ -1,7 +1,12 @@
-use kiss3d::{nalgebra::Point3, window::Window};
+use kiss3d::{
+    camera::ArcBall,
+    nalgebra::{Point3, Vector3},
+    window::{CanvasSetup, Window},
+};
 
 pub trait Render {
     fn draw_axes(&mut self);
+    fn window_and_camera_set_up() -> (Window, ArcBall);
 }
 
 impl Render for Window {
@@ -27,5 +32,27 @@ impl Render for Window {
             &Point3::new(0.0, 0.0, 1.0),
             &Point3::new(0.0, 0.0, 1.0),
         );
+    }
+
+    fn window_and_camera_set_up() -> (Window, ArcBall) {
+        let window = Window::new_with_setup(
+            "Volatility Surface Mesh",
+            800,
+            600,
+            CanvasSetup {
+                samples: kiss3d::window::NumSamples::Eight,
+                vsync: true,
+            },
+        );
+
+        // Define starting camera position - we want to look directly at the centre of the plot from the front (in the x-z plane)
+        let eye: Point3<f32> = Point3::new(0.0, -2.5, 0.0);
+        let at: Point3<f32> = Point3::new(0.5, 0.0, 0.5);
+        let up: Vector3<f32> = Vector3::new(0.0, 0.0, 1.0);
+
+        let mut camera = ArcBall::new(eye, at);
+        camera.set_up_axis(up);
+
+        (window, camera)
     }
 }
